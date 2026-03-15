3a863260441cf70b38ac28bda021b9f28b526034
kunzxn <lequochung.17g@gmail.com>
2026-03-16 02:30:57 +0700

    Add UI Game Over Panel, GameOverManager script và cập nhật GameScene

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    [Header("Kéo GameOverPanel vào đây")]
+    public GameObject gameOverPanel;
+
+    // 1. Hàm hiện UI Game Over
+    public void ShowGameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f; // Đóng băng mọi hoạt động trong game
+    }
+
+    // 2. Hàm cho nút RESTART (Chơi lại map này) 
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; // Xả đông thời gian trước khi load
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // 3. Hàm cho nút QUIT (Về màn hình Menu chính)
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1f; // Xả đông thời gian trước khi chuyển Scene
+        SceneManager.LoadScene("IntroductionScene");
+    }
+}
